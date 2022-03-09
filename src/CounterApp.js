243b0bafc04cb1339e80ed67c353d966a0ca2f2e
edrey-sl <martinsalinasluna@gmail.com>
2022-03-09 00:11:23 -0600

    Primera app CounterApp

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types'
 
 const CounterApp = ({value}) => {
 
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(value);
 
-    const sumar = () =>{
-       
-        setCounter(counter + 1);
-    }
-
+    const sumar = () => setCounter(counter + 1);
+    
+    const resta = () => setCounter(counter - 1);
+ 
+    const reset = () => setCounter(value);
+    
 
   return <>
 
   <h1 > CounterAPP </h1> 
   <h2> {counter} </h2>
   <button onClick={sumar}>Sumar +1</button> 
+  <button onClick={reset}>Reset</button> 
+  <button onClick={resta}>Menos -1</button> 
   </>
 }
 
